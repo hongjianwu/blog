c8deba27a5292b6256f3fc77831d07e6a9ac9cf0
jianwu <jianwu@weiapp.com>
2018-07-20 17:38:59 +0800

    test

--- server.php
+++ server.php
@@ -13,7 +13,7 @@ $serv->on('connect', function(){
 });
 $serv->on('receive', function($serv, $fd, $from_id, $data){
 
-	$serv->send($fd, 'swoole1:'.$data);
+	$serv->send($fd, 'swoole'.$data."  fd:".$fd."  from_id:".$from_id);
 	$serv->close($fd);
 });
 $serv->on('close', function($serv, $fd){
