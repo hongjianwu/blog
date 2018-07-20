33eea3647fe720aa22e3195e4491f2bb4043114a
jianwu <jianwu@weiapp.com>
2018-07-20 17:10:57 +0800

    test

--- client.php
+++ client.php
@@ -1,8 +1,9 @@
 <?php 
-$client = new swoole_client(SWOOLE_SOCK_TCP);
-if (!$client->connect('120.78.51.75', 8080, -1)) {
-	exit("connect failed . Error: {$client->errCode}\n");
-}
-$client->send("hello world\n");
-echo $client->recv();
-$client->close();
+echo 1;exit;
+// $client = new swoole_client(SWOOLE_SOCK_TCP);
+// if (!$client->connect('120.78.51.75', 8080, -1)) {
+// 	exit("connect failed . Error: {$client->errCode}\n");
+// }
+// $client->send("hello world\n");
+// echo $client->recv();
+// $client->close();
