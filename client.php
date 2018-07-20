c808b3233de4a436a919841fe8fb26b3dea7a2e2
jianwu <jianwu@weiapp.com>
2018-07-20 17:03:24 +0800

    test

--- client.php
+++ client.php
@@ -1,22 +1,8 @@
-<?php
+<?php 
 $client = new swoole_client(SWOOLE_SOCK_TCP);
-
-//连接到服务器
-if (!$client->connect('127.0.0.1', 8080, 0.5))
-{
-    die("connect failed.");
+if (!$client->connect('127.0.0.1', 9501, -1)) {
+	exit("connect failed . Error: {$client->errCode}\n");
 }
-//向服务器发送数据
-if (!$client->send("hello world"))
-{
-    die("send failed.");
-}
-//从服务器接收数据
-$data = $client->recv();
-if (!$data)
-{
-    die("recv failed.");
-}
-echo $data;
-//关闭连接
+$client->send("hello world\n");
+echo $client->recv();
 $client->close();
