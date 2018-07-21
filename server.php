1bc494cf0d878251a0f4b4972668b5ba7ca88ef9
jianwu <jianwu@weiapp.com>
2018-07-21 14:48:52 +0800

    test

--- server.php
+++ server.php
@@ -1,24 +1,41 @@
 <?php
 $serv = new swoole_server('0.0.0.0', 8080);
-$serv->set([
-	'reactor' => 3,
-	'worker_num' => 2,
-	'daemonize' => true,
-	'backlog' => 128
-]);
+// $serv->set([
+// 	'reactor' => 3,
+// 	'worker_num' => 2,
+// 	'daemonize' => true,
+// 	'backlog' => 128
+// ]);
 
-$serv->on('connect', function(){
-	echo "client:connect.\n";
+// $serv->on('connect', function(){
+// 	echo "client:connect.\n";
 
-});
-$serv->on('receive', function($serv, $fd, $from_id, $data){
+// });
+// $serv->on('receive', function($serv, $fd, $from_id, $data){
 
-	$serv->send($fd, 'swoole'.$data."  fd:".$fd."  from_id:".$from_id);
-	$serv->close($fd);
+// 	$serv->send($fd, 'swoole'.$data."  fd:".$fd."  from_id:".$from_id);
+// 	$serv->close($fd);
+// });
+// $serv->on('close', function($serv, $fd){
+// 	echo "client:close\n";
+// });
+// $port = $serv->addlistener("0.0.0.0", 8080, SWOOLE_SOCK_TCP);
+// echo $port->port;
+
+
+
+$process = new swoole_server('0.0.0.0', 8080);
+$process = new swoole_process(function($process) use ($server){
+
+	while(true){
+		$msg = $process->read();
+		foreach ($server->connections  as $conn) {
+			$server->send($conn, $msg);
+		}
+	}
 });
-$serv->on('close', function($serv, $fd){
-	echo "client:close\n";
-});
-$port = $serv->addlistener("0.0.0.0", 8080, SWOOLE_SOCK_TCP);
-echo $port->port;
+$server->addProcess($process);
+$server->on('receive', function($serv, $fd, $from_id, $data) use ($process){
+	$process->write($data);
+})
 $serv->start();
