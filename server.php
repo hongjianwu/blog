3eaf086c7f40845fca8d20a1c4b40e48ed754710
jianwu <jianwu@weiapp.com>
2018-07-24 17:11:54 +0800

    test

--- server.php
+++ server.php
@@ -5,15 +5,17 @@ $server->on('open', function(swoole_websocket_server $server, $request){
 });
 $server->on('message', function(swoole_websocket_server $server, $frame){
 	echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode}, find:{$frame->finish}\n";
-	$server->push($frame->fd, $frame->data);
-});
-$server->on('close' , function($ser, $fd){
-	echo "client {$fd} closed\n";
-});
-$server->on('request', function(swoole_http_request $request, swoole_http_response $response){
-	global $server;
 	foreach ($server->connections  as $fd) {
 		$server->push($fd, $resquest->get['message']);
 	}
 });
+$server->on('close' , function($ser, $fd){
+	echo "client {$fd} closed\n";
+});
+// $server->on('request', function(swoole_http_request $request, swoole_http_response $response){
+// 	//global $server;
+// 	foreach ($server->connections  as $fd) {
+// 		$server->push($fd, $resquest->get['message']);
+// 	}
+// });
 $server->start();
